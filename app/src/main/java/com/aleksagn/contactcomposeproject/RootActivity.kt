3425f8ddbd78d56aac285a717fd43842f8f73aa5
Aleksandr Nomokonov <aleks.agn@gmail.com>
2026-02-06 02:56:16 +0900

    zero commit

---
+++ app/src/main/java/com/aleksagn/contactcomposeproject/RootActivity.kt
@@ -0,0 +1,11 @@
+package com.aleksagn.contactcomposeproject
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class RootActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
